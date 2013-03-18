40b8603798fb9ba862aeec719c99ced6127d3cff
Lev Walkin <vlm@lionet.info>
2013-03-18 05:03:01 -0700

    foursquare checkins

---
+++ task-8/my/generic-restaurant-model.js
@@ -0,0 +1,42 @@
+var GenericRestaurant = Backbone.Model.extend({
+
+    defaults: {
+        state: "open",
+        currentOccupancy: 0
+    },
+
+    validate: function(attributes) {
+        switch(attributes.state) {
+        case "open":
+            if(attributes.currentOccupancy < 0)
+                return "Number of visitors must not be negative";
+            break;
+        case "closed":
+            if(attributes.currentOccupancy == 0)
+                return "Number of visitors in a closed restaurant must be 0";
+            break;
+        default:
+            return "Expected 'open' or 'close' state";
+        }
+    },
+
+    visitorCame: function() {
+        return this.set({currentOccupancy: this.get("currentOccupancy") + 1 },
+                        {validate: true});
+    },
+
+    visitorLeft: function() {
+        return this.set({currentOccupancy: this.get("currentOccupancy") - 1 },
+                        {validate: true});
+    },
+
+    closeRestaurant: function() {
+        if(this.get("state") == "open") {
+            if(this.get("currentOccupancy") == 0)
+                this.set("state", "closed");
+            else
+                throw "Can't close restaurant which still has visitors!";
+        }
+    }
+});
+
